130acab5a0ec64c0316c7f49a2f74610a80c68c9
marco <mpjim20@gmail.com>
2023-03-06 10:53:59 +0100

    .

--- scripts/q_learning_temp_conv.py
+++ scripts/q_learning_temp_conv.py
@@ -326,7 +326,7 @@ if __name__=='__main__':
     if continue_:
 
         if opt.savedir is None:
-            filepath = '/homes/mjimenezhaertel/Masterarbeit/Training/' 
+            filepath = '/home/marco/Uni/Masterarbeit/Training/' 
         else:
             filepath = opt.savedir 
     
