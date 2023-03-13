72894aab518ca9ca3dbdb2b8f8fb4cf80b2457a8
marco <mpjim20@gmail.com>
2023-03-13 15:17:01 +0100

    fixed tableheight

--- scripts/q_learning_temp_conv.py
+++ scripts/q_learning_temp_conv.py
@@ -120,7 +120,7 @@ class DQN_Algo():
         else:
             self.stepcount = 0
         
-        self.tableheight = 0.4
+        self.tableheight = 0.15
 
         self.n_timesteps = n_timesteps
         self.cur_state_stack = State(state_myrmex=deque([torch.zeros((1,1,2,16,16), dtype=torch.double, device=self.device) for _ in range(self.n_timesteps)], maxlen=self.n_timesteps),
@@ -265,7 +265,7 @@ class DQN_Algo():
             if episode % 5 == 0:
                 r = self.test()
                 if r > 0:
-                    self.tableheight -= 0.1
+                    self.tableheight -= 0.05
                     if self.tableheight < 0.01:
                         self.tableheight = 0.01
                         
