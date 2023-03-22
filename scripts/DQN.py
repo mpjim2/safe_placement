d1668cc10958ca2cb1fbcab1583aff75a7116c5d
marco <mpjim20@gmail.com>
2023-03-22 15:13:24 +0100

    added NN architecture for fingertip sensor

--- scripts/DQN.py
+++ scripts/DQN.py
@@ -7,46 +7,8 @@ from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 
 #Basic Network, Only takes latest timestep as input
-class place_net(nn.Module):
-    
-    def __init__(self, n_actions):
-        super(place_net, self).__init__()
-        
-        #Convolutional network for feature extraction of Myrmex sensors
-        self.myrmex_ftrs = torch.nn.Sequential(
-            torch.nn.Conv2d(1, 8, 8),
-            torch.nn.Conv2d(8, 4, 4),
-        )
 
-        self.feedforward = torch.nn.Sequential(
-            torch.nn.Linear(in_features=315, out_features=256),
-            torch.nn.ReLU(),
-            torch.nn.Linear(in_features=256, out_features=128),
-            torch.nn.ReLU(),
-            torch.nn.Linear(in_features=128, out_features=64),
-            torch.nn.ReLU(),
-            torch.nn.Linear(64, n_actions),
-            torch.nn.Softmax(dim=0)
-        )
-
-    def forward(self, myrmex_r, 
-                      myrmex_l, 
-                      pose, 
-                      j_pos, 
-                      j_tau, 
-                      j_vel):
-        
-        myr1 = torch.flatten(self.myrmex_ftrs(myrmex_r), start_dim=1)
-        myr2 = torch.flatten(self.myrmex_ftrs(myrmex_l), start_dim=1)
-        
-        myr_c = torch.cat((myr1, myr2, pose, j_pos, j_tau, j_vel), 1)
-
-        action_probs = self.feedforward(myr_c)
-
-        
-        return action_probs
-
-#Takes history of last 20 Timesteps as input
+#Takes history of last n Timesteps as input
 class MAT_based_net(nn.Module):
     
     def __init__(self, n_actions, n_timesteps):
@@ -167,27 +129,39 @@ class Conv2Plus1D(nn.Module):
 
 class placenet_v2(nn.Module):
 
-    def __init__(self, n_actions, n_timesteps) -> None:
+    def __init__(self, n_actions, n_timesteps, sensor_type) -> None:
         super().__init__()
 
-        self.tactile_ftrs = nn.Sequential(
-            Conv2Plus1D(in_channels=2, 
-                        out_channels=16,
-                        spatial_size=7,
-                        temporal_size=5),
-            nn.ReLU(),
-            Conv2Plus1D(in_channels=16,
-                        out_channels=32,
-                        spatial_size=5,
-                        temporal_size=3),
-            nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(4608, 128),
-            nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU()
-            )
-
+        if sensor_type == 'plate':
+            self.tactile_ftrs = nn.Sequential(
+                Conv2Plus1D(in_channels=2, 
+                            out_channels=16,
+                            spatial_size=7,
+                            temporal_size=5),
+                nn.ReLU(),
+                Conv2Plus1D(in_channels=16,
+                            out_channels=32,
+                            spatial_size=5,
+                            temporal_size=3),
+                nn.ReLU(),
+                nn.Flatten(),
+                nn.Linear(4608, 128),
+                nn.ReLU(),
+                nn.Linear(128, 128),
+                nn.ReLU()
+                )
+        elif sensor_type =='fingertip':
+            
+            self.tactile_ftrs = nn.Sequential(
+                nn.Conv2d(2, 1, (5, 1), padding=0), 
+                nn.ReLU(),
+                nn.Flatten(),
+                nn.Linear(72, 128),
+                nn.ReLU(),
+                nn.Linear(128, 128),
+                nn.ReLU()
+                )
+            
         self.pose_embedding = nn.Sequential(
             nn.Conv2d(1, 1, (5, 1), padding=0), 
             nn.ReLU(),
@@ -256,7 +230,7 @@ if __name__=='__main__':
  
 
 
-    x = torch.rand((1, 1, 10, 7))
+    x = torch.rand((1, 1, 10, 12))
 
     layer = nn.Conv2d(1, 1, (5, 1), padding=0)
 
