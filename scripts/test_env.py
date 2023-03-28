71ea1cd3b654e8601e2fb33501d60e2ac82b2ec8
marco <mpjim20@gmail.com>
2023-03-28 14:13:25 +0200

    debugging

--- scripts/test_env.py
+++ scripts/test_env.py
@@ -8,7 +8,7 @@ import numpy as np
 
 if __name__=="__main__":
 
-    env = gym.make('TactileObjectPlacementEnv-v0', sensor="plate", continuous=False)
+    env = gym.make('TactileObjectPlacementEnv-v0', sensor="fingertip", continuous=False)
 
     # env.reset_world()
     # for i in range(10):
@@ -20,12 +20,26 @@ if __name__=="__main__":
     # env.reset(options={'min_table_height' : 0.1, 'testing' :True})
     case_found = False
     n = env.action_space.n
-    for action in range(1):
-        env.reset(options={'gap_size' : 0.002, 'angle_range' : 0.17,'testing' :True})
+
+    g = 0.002
+    ar = 0.17
+
+    for action in range(10000):
+        
+        print('########################')
+        print(action)
+        print('########################')
+
+        env.reset(options={'gap_size' : 0.002, 'angle_range' : 0.17, 'testing' : False})
         done = False
-        time.sleep(3)
+        time.sleep(0.5)
+        if action %100 == 0:
+            g += 0.002
+            ar += 0.05
+        
+        # time.sleep(3)
         # while not done:
-        #     _, r, done, _, _ = env.step(action)
+        #     _, r, done, _, _ = env.step(3)
             
         #     if r == -0.5:
         #         case_found = True
